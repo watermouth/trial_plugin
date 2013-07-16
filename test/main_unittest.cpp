40bccec80f29cfe1dc14e50fed56faf20540c4bc
watermouth <sminakuchi@gmail.com>
2013-07-16 22:01:48 +0900

    omakable gtestBuild

---
+++ test/main_unittest.cpp
@@ -0,0 +1,17 @@
+#include "gtest/gtest.h"
+#include "pluginmanager.h"
+#include "pluggable_class_factory.h"
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+TEST(Main, GetAllPlugins){
+  vector<PluginInstance*> plugins = PluginManager::GetInstance().GetAllPlugins();
+  for(vector<PluginInstance*>::iterator it = plugins.begin();
+    it != plugins.end(); ++it) {
+    cout << (**it).GetFileName() << endl;
+  } 
+}
+
